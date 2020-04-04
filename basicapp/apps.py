d8e89e808423ea92aff5cbe00aebd2888a758a14
gcurtis1234 <gcurtis1234@gmail.com>
2020-04-04 13:08:48 -0500

    inital commit

---
+++ basicapp/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class BacicappConfig(AppConfig):
+    name = 'bacicapp'
---
+++ basicapp/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+
+# Create your views here.
+def index(request):
+
+    return render(request,'basic_app/index.html')
+
+
+def other(request):
+    return render(request,'basic_app/other.html')
+
+def rel(request):
+    return render(request,'basic_app/rel.html')
